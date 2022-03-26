57b8c2592433e829cda781424e041303c913368a
Mustafa Omar <M@Mustafas-MacBook-Pro.local>
2022-03-26 13:24:53 +0000

    Minor

--- APP/purejax_cnn/main.py
+++ APP/purejax_cnn/main.py
@@ -35,11 +35,11 @@ example_batch_x,example_batch_y = training_object.Load_batch(train[0], data_shap
 #model initialisation
 print('-> Model init')
 init_fun, apply_fun = my_combinator(
-    stax.Conv(16,(10,10), padding='SAME'),Relu_layer,
+    stax.Conv(16,(5,5), padding='SAME'),Relu_layer,
     stax.MaxPool((3,3)),
-    stax.Conv(16,(10,10), padding='SAME'),Relu_layer,
+    stax.Conv(16,(5,5), padding='SAME'),Relu_layer,
     stax.AvgPool((3,3)),
-    stax.Conv(16, (5,5),padding='SAME'),Relu_layer,
+    stax.Conv(16, (3,3),padding='SAME'),Relu_layer,
     stax.MaxPool((5,5)),
 
     my_Flatten(),
