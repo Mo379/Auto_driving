ea8de830db1fe8e644855d4dff74e7453675cc98
Mustafa Omar <M@Mustafas-MacBook-Pro.local>
2022-03-26 13:57:49 +0000

    minor

--- APP/purejax_cnn/main.py
+++ APP/purejax_cnn/main.py
@@ -35,17 +35,16 @@ example_batch_x,example_batch_y = training_object.Load_batch(train[0], data_shap
 #model initialisation
 print('-> Model init')
 init_fun, apply_fun = my_combinator(
-    stax.Conv(5,(5,5), padding='SAME'),Relu_layer,
+    stax.Conv(8,(5,5), padding='SAME'),Relu_layer,
     stax.MaxPool((3,3)),
-    stax.Conv(5,(5,5), padding='SAME'),Relu_layer,
+    stax.Conv(8,(5,5), padding='SAME'),Relu_layer,
     stax.AvgPool((3,3)),
-    stax.Conv(5, (3,3),padding='SAME'),Relu_layer,stax.BatchNorm(),
+    stax.Conv(8, (3,3),padding='SAME'),Relu_layer,
     stax.MaxPool((5,5)),
-    stax.Conv(5, (3,3),padding='SAME'),Relu_layer,
+    stax.Conv(8, (3,3),padding='SAME'),Relu_layer,
     stax.AvgPool((5,5)),
 
     my_Flatten(),
-    my_Dense(64),
     my_Dense(2)
 )
 input_shape =example_batch_x.shape[-3:]
