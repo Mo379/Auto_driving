cd2a0c078dbc0357b62816d118b942094a38fc63
Mustafa Omar <M@Mustafas-MacBook-Pro.local>
2022-04-11 11:46:34 +0100

    Minor

---
+++ APP/purejax_transfer_mobilenet/main.py
@@ -0,0 +1,136 @@
+import wandb
+import pickle
+import time
+import jax
+from jax.example_libraries import stax, optimizers
+import functools
+from src.datahandle import *
+from src.model_cnn import *
+import time
+import pandas as pd
+import random
+#hyper
+directory = '../../extras/data/A_training_given/training_data/'
+training_folder = 'training_data'
+training_labels_file= 'training_norm.csv'
+#
+quiz_directory = '../../extras/data/C_testing_given/test_data/'
+quiz_training_folder = 'test_data'
+#configurations
+conf_tracking = 1
+seed = 4
+rng = jax.random.PRNGKey(seed)
+rng2 = jax.random.PRNGKey(seed)
+data_shape = 'original'
+parameter_init_scale = 0.01
+split= 0.8
+batch_size = 128 
+n_epochs = 15
+lr = 0.0005
+#dataloading object
+training_object= DataLoader(
+        directory,
+        training_folder,
+        training_labels_file
+        )
+#train test split
+train,test = training_object.LoadModelData_info(
+        split = split, 
+        batch_size = batch_size)
+example_batch_x,example_batch_y = training_object.Load_batch(train[0], data_shape=data_shape)
+#model initialisation
+test = test[:,:-7,:].reshape(10,-1,4)
+print('-> Model init')
+def make_net(mode: str):
+    return stax.serial( 
+        stax.Conv(5,(6,6), padding='SAME'),LeakyRelu_layer,stax.Dropout(0.2, mode=mode),
+        stax.MaxPool((3,3)),
+
+        stax.Conv(5,(6,6), padding='SAME'),LeakyRelu_layer,stax.Dropout(0.2, mode=mode),
+        stax.MaxPool((3,3)),
+        
+        stax.Conv(5, (6,6),padding='SAME'),LeakyRelu_layer,stax.Dropout(0.2, mode=mode),
+        stax.MaxPool((3,3)),
+
+        stax.Conv(5, (6,6),padding='SAME'),LeakyRelu_layer,stax.Dropout(0.2, mode=mode),
+        stax.MaxPool((3,3)),
+
+        my_Flatten(),
+        my_Dense(2)
+    )
+init_fun, apply_fun = make_net('train')
+input_shape =example_batch_x.shape[-3:]
+_, params= init_fun(rng, (batch_size,) + input_shape)
+model_shape = jax.tree_map(lambda x: x.shape, params)
+
+
+#wandb tracking
+if conf_tracking:
+    config = {
+      "model_type" : 'Bigger convnet + avg pool and max pool layers, batch normalisation and dropout, using my own data generated from the car and data augmentation, Realu swapped for leaky relu',
+      "param_initialisation_scale" : parameter_init_scale,
+      "model_shape" : str(model_shape),
+      "learning_rate": lr,
+      "data_split": split,
+      "batch_size": batch_size,
+      "data_shape" : str(data_shape),
+      "epochs": n_epochs,
+      "Note" : "This run focuses on adding the testing data loss, and improving workflow by making predictions directly after training. The cnn sees improvments by adding additional layers such as batch normalisation and dropout. Relu swapped with LeakyRelu"
+    }
+    wandb.init(project="Autonomous-driving", entity="mo379",config=config)
+# 
+@jax.jit
+def loss_fn(params, x, y):
+    predictions = apply_fun(params,x,rng=rng2)
+    return (1/len(x))*jnp.sum((predictions-y)**2)
+@jax.jit
+def update(opt_state, x,y):
+    loss, grads = jax.value_and_grad(loss_fn)(opt_get_params(opt_state), 
+            x,y)
+    opt_state =opt_update(i, grads, opt_state)
+    return loss,opt_state
+#optimizer_init
+print('-> Optimizer init')
+opt_init, opt_update, opt_get_params = optimizers.rmsprop_momentum(lr)
+opt_state = opt_init(params)
+##################GPU goes buuurrrrrr#######################
+if __name__ == "__main__":
+    print("Begin training")
+    start = time.time()
+    for epoch in range(n_epochs):
+        for i in range(len(train)): 
+            X_batch,Y_batch=training_object.Load_batch(train[i], data_shape=data_shape) 
+            #X_batch,Y_batch = training_object.Augment_batch(X_batch,Y_batch)
+            loss, opt_state = update(opt_state, X_batch, Y_batch)
+            print(f"- Batch {i} at loss: {loss}")
+            if conf_tracking==1:
+                test_x, test_y=training_object.Load_batch(test[random.randint(0, 9)], 
+                        data_shape=data_shape)
+                test_loss = loss_fn(opt_get_params(opt_state), test_x,test_y)
+                wandb.log({"test_loss": test_loss})
+                wandb.log({"batch_loss": loss})
+        print(f"--- Epoch {epoch} at loss {loss}")
+    end = time.time()
+    print(f"total time: {end-start}")
+    pickle.dump(opt_get_params(opt_state), open('pkls/final_params.pkl', 'wb'))
+    print("-> Making predictions")
+    init_fun, apply_fun = make_net('test')
+
+    quiz_object= DataLoader(
+        quiz_directory,
+        quiz_training_folder,
+        )
+    quiz_train = quiz_object.LoadQuizData_info()
+    X,image_order = quiz_object.Load_batch_quiz(quiz_train,data_shape=data_shape)
+    prds = np.array(apply_fun(params,X,rng=rng2))
+    final_prd = np.column_stack((image_order,prds))
+    final_ordered = final_prd[final_prd[:, 0].argsort()]
+    df = pd.DataFrame(final_ordered, columns = ['image_id','angle','speed'])
+    df = df.astype({'image_id': 'int32'})
+    df.to_csv('submission.csv', index=False,)
+
+
+
+
+
+
---
+++ APP/purejax_transfer_mobilenet/src/datahandle.py
@@ -0,0 +1,164 @@
+import os
+import sys
+import glob
+import numpy as np
+from numpy import genfromtxt
+from PIL import Image
+from numpy import asarray
+import pandas as pd
+import matplotlib.pyplot as plt
+import jax.numpy as jnp
+
+class DataLoader:
+    def __init__(self, directory, images_dir,labels_file=''):
+        self.directory = directory
+        self.images_dir = images_dir
+        self.labels_file= labels_file
+
+    def Load_all_batches(self, data, data_shape=(-1,1)):
+        loaded_set_features = []
+        loaded_set_labels= []
+        for batch in data:
+            X = []
+            Y = []
+            for instance in batch:
+                link = instance[0]
+                image = Image.open(link)
+                data = asarray(image)
+                if data.shape[2] == 4:
+                    data = data[:,:,:-1]
+                if data_shape != 'original':
+                    data = data.reshape(data_shape)
+                X.append(data)
+                image_number = instance[1]
+                angle = instance[2]
+                speed = instance[3]
+                Labels = np.array([angle,speed], dtype=np.float32)
+                Y.append(Labels)
+            loaded_set_features.append(X)
+            loaded_set_labels.append(Y)
+        loaded_set_features = jnp.array(loaded_set_features,dtype=jnp.float32)/255
+        loaded_set_labels= jnp.array(loaded_set_labels,dtype=jnp.float32)/255
+        return loaded_set_features,loaded_set_labels
+
+    def Load_batch(self, batch, data_shape=(-1,1)):
+        X = []
+        Y = []
+        for instance in batch:
+            link = instance[0]
+            image = Image.open(link)
+            data = asarray(image)
+            if data.shape[2] == 4:
+                data = data[:,:,:-1]
+            if data_shape != 'original':
+                data = data.reshape(data_shape)
+            X.append(data)
+            image_number = instance[1]
+            angle = instance[2]
+            speed = instance[3]
+            Labels = np.array([angle,speed], dtype=np.float32)
+            Y.append(Labels)
+        loaded_set_features = jnp.array(X,dtype=jnp.float32)/255
+        loaded_set_labels= jnp.array(Y,dtype=jnp.float32)
+        return loaded_set_features,loaded_set_labels
+
+    def Load_batch_quiz(self, batch, data_shape=(-1,1)):
+        X = []
+        Image_order = []
+        for instance in batch:
+            link = instance[0]
+            image = Image.open(link)
+            data = asarray(image)
+            if data.shape[2] == 4:
+                data = data[:,:,:-1]
+            if data_shape != 'original':
+                data = data.reshape(data_shape)
+            image_number = instance[1]
+            X.append(data)
+            Image_order.append(image_number)
+        X = jnp.array(X)/255
+        Image_order = np.array(Image_order)
+        return X,Image_order
+
+    def LoadModelData_info(self, split, batch_size):
+        self._get_imgsinfo_train()
+        if split:
+            self._train_test_split(split=split)
+        if batch_size:
+            self._batch_imgsinfo_train(batch_size=batch_size)
+        return self.train_images_information, np.array([self.test_images_information])
+
+    def LoadQuizData_info(self):
+        self._get_imgsinfo_test()
+        return self.quiz_images_information
+
+    def _train_test_split(self, split):
+        n = len(self.train_images_information)
+        split_index = int(np.ceil(n * split))
+        train_split = self.train_images_information[0:split_index]
+        test_split = self.train_images_information[split_index+1:n]
+        self.train_images_information = train_split
+        self.test_images_information = test_split
+
+    def _batch_imgsinfo_train(self, batch_size):
+        n = len(self.train_images_information)
+        if n % batch_size ==0:
+            batched_imgsinfo_train = np.reshape(self.train_images_information,
+                    (int(n/batch_size),-1,4)
+                    )
+        else: 
+            while n % batch_size != 0:
+                n -= 1
+            batched_imgsinfo_train = np.reshape(
+                    self.train_images_information[0:n],
+                    (int(n/batch_size),-1,4)
+                    )
+        self.train_images_information = batched_imgsinfo_train
+
+
+    def _get_imgsinfo_train(self):
+        #Get the absolute paths of the data
+        absolute_paths = glob.glob(self.directory+self.images_dir+"/*.png") 
+        #getting the labels
+        labels = self._get_labels()
+        df = pd.DataFrame(labels, columns = ['index','angle','speed'])
+        df = df.set_index('index')
+        self.train_images_information= []
+        for path in absolute_paths:
+            parts= path.split('/')
+            image_name = parts[-1]
+            image_number = int(image_name.split('.')[0])
+            if image_number in df.index:
+                image_label = df.loc[image_number]
+            else:
+                continue
+            angle = image_label[0]
+            speed = image_label[1]
+            information =[ 
+                    path,
+                    image_number,
+                    angle,
+                    speed
+                    ]
+            self.train_images_information.append(information)
+
+    def _get_imgsinfo_test(self):
+        #Get the absolute paths of the data
+        absolute_paths = glob.glob(self.directory+self.images_dir+"/*.png") 
+        #getting the labels
+        self.quiz_images_information= []
+        for path in absolute_paths:
+            parts= path.split('/')
+            image_name = parts[-1]
+            image_number = int(image_name.split('.')[0])
+            information =[ 
+                    path,
+                    image_number,
+                    ]
+            self.quiz_images_information.append(information)
+
+    def _get_labels(self):
+        labels = genfromtxt(self.directory+self.labels_file , delimiter=',' , skip_header=1, dtype=np.float32) 
+        return labels
+
+
